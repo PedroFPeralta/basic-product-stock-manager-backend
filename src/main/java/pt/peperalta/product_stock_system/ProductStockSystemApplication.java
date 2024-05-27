de3511030d1efd87555636977b927f972fe17087
PedroFPeralta <200221137@estudantes.ips.pt>
2024-05-27 21:23:37 +0100

    Added: Initial Commit

---
+++ src/main/java/pt/peperalta/product_stock_system/ProductStockSystemApplication.java
@@ -0,0 +1,13 @@
+package pt.peperalta.product_stock_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStockSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStockSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/peperalta/product_stock_system/ProductStockSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.peperalta.product_stock_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStockSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
